71a1ef1431c6f28e7128e057515297a19aa7298d
lrn0409 <linrn0409@gmail.com>
2016-10-07 23:54:00 +0800

    Update cluster2.lua

--- cluster/cluster2.lua
+++ cluster/cluster2.lua
@@ -2,6 +2,8 @@ local skynet = require "skynet"
 local cluster = require "cluster"
 
 skynet.start(function()
-    local proxy = cluster.proxy("db","sdb")
+	local sdb = cluster.query("db", "sdb")	
+    local proxy = cluster.proxy("db",sdb)
+	--print(cluster.call("db", sdb, "GET", "a"))    
     print(skynet.call(proxy, "lua", "GET", "a"))
 end)
